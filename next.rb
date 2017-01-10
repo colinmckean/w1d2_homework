47600ff4b548e0d3efd6e77edaa0938fc7cb99d5
Colin McKean <colinmckeandigital@gmail.com>
2017-01-10 15:49:54 +0000

    add short_month function

--- next.rb
+++ next.rb
@@ -56,3 +56,34 @@ def long_months(month_number)
     "enter valid number for month"
   end
 end
+
+def short_month(month_number)
+  case month_number
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  else
+    return "enter valid number for month"
+  end
+end
